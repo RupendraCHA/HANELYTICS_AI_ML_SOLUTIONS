8e0f1c7bf0ecd0f9233a24f6b67a3ab41fc10f05
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-11-05 22:23:30 +0530

    Changing Styles

--- client/src/Charts/RevenueCharts/RevenuePieChart/RevenuePieChart.jsx
+++ client/src/Charts/RevenueCharts/RevenuePieChart/RevenuePieChart.jsx
@@ -19,14 +19,19 @@ function RevenuePieChart({ data }) {
         borderColors.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
     })
 
+    const bgColor1 = ["#785532","#43ecab","#3cec45","#f6bffb","#c075d" ,"#21f244","#83059a","#dc1450","#6afbc8","#561eee","#6adff2","#c94926","#49607c","#93ac78","#a3febb","#e3a2dc","#e2072a","#5cde47","#63132e","#5166c2","#9cd047"]
+
+    const bgColors2 = [ "#c4f1ba", "#bbee0a", "#4cb7f1", "#d0c691", "#167505", "#86ca03", "#51356d", "#aa59e7", "#fef291", "#dceb85","#ddcc5f","#171d13","#837d8e","#7382a7","#e09069","#a47da0","#773759","#60e6aa","#b0f2fe","#73e2b3"
+    ]
+
     const [userData1, setUserData1] = useState({
         // 
         labels: slicedData.map((data) => data.Item_SKU),
         datasets: [{
             // label: "Sales Data",
             data: slicedData.map((data) => data.Predicted_Revenue_for_Upcoming_90_Days),
-            backgroundColor: backgroundColors1,
-            borderColor: backgroundColors1,
+            backgroundColor: bgColor1,
+            borderColor: "white",
             borderWidth: 1,
             hoverOffset: 30,
         }
